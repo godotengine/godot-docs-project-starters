8c60d32aea0f8beb6bd49337e45117c4b23d188a
Hana - Piralein <48352564+Piralein@users.noreply.github.com>
2023-04-27 15:22:32 +0200

    update instancing example

--- src/instancing_starter/Main.gd
+++ src/instancing_starter/Main.gd
@@ -1,9 +1,9 @@
 extends Node
 
-export (PackedScene) var Ball
+@export var Ball: PackedScene
 
 func _input(event):
 	if event.is_action_pressed("click"):
-		var new_ball = Ball.instance()
+		var new_ball = Ball.instantiate()
 		new_ball.position = get_viewport().get_mouse_position()
 		add_child(new_ball)
