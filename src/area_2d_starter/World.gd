6aab80446ba639b11f5da3f2a6194824e01e865c
Hana - Piralein <48352564+Piralein@users.noreply.github.com>
2023-08-20 16:05:52 +0200

    update area_2d starter to 4.x

--- src/area_2d_starter/World.gd
+++ src/area_2d_starter/World.gd
@@ -6,16 +6,16 @@ var Ball = load("res://Ball.tscn")
 func _draw():
 	# Draw shaded regions to show the areas.
 	draw_circle($Point.position, $Point/CollisionShape2D.shape.radius,
-				Color(1, 1, 1, 0.1))	
-	var s = $Antigrav/CollisionShape2D.shape.extents
-	draw_rect(Rect2($Antigrav.position-s, s * 2), Color(1, 1, 0, 0.1))
-	s = $Slow/CollisionShape2D.shape.extents
-	draw_rect(Rect2($Slow.position-s, s * 2), Color(0, 1, 1, 0.1))
+				Color(1, 1, 1, 0.1))
+	var s = $Antigrav/CollisionShape2D.shape.size
+	draw_rect(Rect2($Antigrav.position - (s / 2), s), Color(1, 1, 0, 0.1))
+	s = $Slow/CollisionShape2D.shape.size
+	draw_rect(Rect2($Slow.position - (s / 2), s), Color(0, 1, 1, 0.1))
 
 	
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == BUTTON_LEFT:
-			var b = Ball.instance()
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var b = Ball.instantiate()
 			b.position = event.position
 			add_child(b)
